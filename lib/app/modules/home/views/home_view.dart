f56db1dbe781df485e6443fcbb6f3c416b479c5a
SHC12 <superhumancrewid12@gmail.com>
2022-05-25 01:06:23 +0700

    feat: added detail movie, added review

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -31,6 +31,7 @@ class _HomeViewState extends State<HomeView> {
     super.initState();
     movieController.fetchNowPlaying();
     movieController.fetchPopular();
+    movieController.fetchUpcoming();
   }
 
   Future refresh() async {
@@ -76,39 +77,6 @@ class _HomeViewState extends State<HomeView> {
                       ],
                     ),
                   ),
-                  SizedBox(
-                    height: Get.height * 0.005,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24),
-                    child: Container(
-                      decoration: BoxDecoration(color: Colors.black12, borderRadius: BorderRadius.circular(8)),
-                      child: GestureDetector(
-                        onTap: () {
-                          // Get.to(SearchMoviePage());
-                        },
-                        child: TextField(
-                          onSubmitted: (value) {
-                            // Get.to(SearchMoviePage());
-                          },
-                          style: whiteTextFont,
-                          controller: tSearch,
-                          enabled: false,
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.search, color: Colors.white),
-                              labelStyle: whiteTextFont,
-                              hintText: "Cari....",
-                              hintStyle: whiteTextFont,
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white), borderRadius: BorderRadius.circular(8))),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 1.h,
-                  ),
                   Container(
                     // padding: EdgeInsets.only(left: 24),
                     child: Column(
@@ -135,10 +103,10 @@ class _HomeViewState extends State<HomeView> {
                                           child: LargePosterCard(
                                             movieController.listNowPlaying[index].posterPath!,
                                             onTap: () {
-                                              // movieController.fetchDetailMovie(
-                                              //     movieController.listNowPlaying[index].id.toString(),
-                                              //     isNowPlaying: true);
-                                              // print(movieController.listNowPlaying[index].id);
+                                              movieController.fetchDetailMovie(
+                                                  movieController.listNowPlaying[index].id.toString(),
+                                                  isNowPlaying: true);
+                                              print(movieController.listNowPlaying[index].id);
                                             },
                                           ),
                                         );
@@ -186,8 +154,6 @@ class _HomeViewState extends State<HomeView> {
                     height: 2.h,
                   ),
                   Container(
-                    // padding: EdgeInsets.only(left: 24),
-
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -256,146 +222,80 @@ class _HomeViewState extends State<HomeView> {
                       ],
                     ),
                   ),
-                  // Obx(() {
-                  //   return Visibility(
-                  //     visible: !nobarController.isLoading.value && nobarController.listNobar.length == 0 ? false : true,
-                  //     child: Container(
-                  //       // padding: EdgeInsets.only(left: 24),
-                  //       child: Column(
-                  //         crossAxisAlignment: CrossAxisAlignment.start,
-                  //         children: [
-                  //           Padding(
-                  //             padding: const EdgeInsets.only(left: 24, bottom: 24),
-                  //             child: Text('Nobar',
-                  //                 style: whiteTextFont.copyWith(
-                  //                     fontSize: Get.height * 0.024, fontWeight: FontWeight.w500)),
-                  //           ),
-                  //           !nobarController.isLoading.value
-                  //               ? CarouselSlider.builder(
-                  //                   itemCount: nobarController.listNobar.length > 10
-                  //                       ? nobarController.listNobar.sublist(0, 10)
-                  //                       : nobarController.listNobar.length,
-                  //                   itemBuilder: (context, index, realIdx) {
-                  //                     return Container(
-                  //                         margin: EdgeInsets.only(
-                  //                             left: (index == 0) ? 0 : 0, right: (index == 3 - 1) ? defaultMargin : 16),
-                  //                         child: NobarCard(
-                  //                           nobarController.listNobar[index],
-                  //                           onTap: () {
-                  //                             nobarController
-                  //                                 .fetchParticipant(nobarController.listNobar[index].idNobar);
-
-                  //                             Get.to(DetailNobarPage(nobarController.listNobar[index]));
-                  //                           },
-                  //                         ));
-                  //                   },
-                  //                   options: CarouselOptions(
-                  //                     height: Get.height * 0.24,
-                  //                     enableInfiniteScroll: false,
-                  //                     viewportFraction: 0.90,
-                  //                     disableCenter: true,
-                  //                     initialPage: 0,
-                  //                     onPageChanged: (index, reason) {
-                  //                       setState(() {
-                  //                         _currentSliderNowPlaying = index;
-                  //                       });
-                  //                     },
-                  //                     scrollDirection: Axis.horizontal,
-                  //                   ),
-                  //                 )
-                  //               : Shimmer.fromColors(
-                  //                   child: CarouselSlider.builder(
-                  //                     itemCount: 3,
-                  //                     itemBuilder: (context, index, relIdx) {
-                  //                       return Container(
-                  //                         margin: EdgeInsets.only(
-                  //                             left: (index == 0) ? 0 : 0, right: (index == 3 - 1) ? defaultMargin : 16),
-                  //                         child: Container(
-                  //                           decoration: BoxDecoration(
-                  //                               color: Colors.white10,
-                  //                               borderRadius: BorderRadius.all(Radius.circular(8))),
-                  //                           width: Get.width * 0.6,
-                  //                         ),
-                  //                       );
-                  //                     },
-                  //                     options: CarouselOptions(
-                  //                       height: MediaQuery.of(context).size.height * 0.24,
-                  //                       enableInfiniteScroll: false,
-                  //                       viewportFraction: 0.90,
-                  //                       initialPage: 0,
-                  //                       disableCenter: true,
-                  //                       onPageChanged: (index, reason) {
-                  //                         setState(() {
-                  //                           _currentSliderNowPlaying = index;
-                  //                         });
-                  //                       },
-                  //                       scrollDirection: Axis.horizontal,
-                  //                     ),
-                  //                   ),
-                  //                   baseColor: Colors.white60,
-                  //                   highlightColor: Colors.grey[300],
-                  //                 )
-                  //         ],
-                  //       ),
-                  //     ),
-                  //   );
-                  // }),
-                  // SizedBox(
-                  //   height: 24,
-                  // ),
-                  // Obx(
-                  //   () => Visibility(
-                  //     visible: !movieController.isLoading.value && movieController.listRamaiReview.length == 0
-                  //         ? false
-                  //         : true,
-                  //     child: Container(
-                  //       // padding: EdgeInsets.only(left: 24),
-                  //       child: Column(
-                  //         crossAxisAlignment: CrossAxisAlignment.start,
-                  //         children: [
-                  //           Padding(
-                  //             padding: const EdgeInsets.only(left: 24, bottom: 24),
-                  //             child: Text('Ramai di review',
-                  //                 style: whiteTextFont.copyWith(
-                  //                     fontSize: Get.height * 0.024, fontWeight: FontWeight.w500)),
-                  //           ),
-                  //           Container(
-                  //             child: Column(
-                  //               children: movieController.listRamaiReview.length <= 10
-                  //                   ? movieController.listRamaiReview.map((element) {
-                  //                       return Padding(
-                  //                         padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 24),
-                  //                         child: WidthMovieCard(
-                  //                           element,
-                  //                           onTap: () {
-                  //                             print(element.id.toString());
-                  //                             movieController.fetchDetailMovie(element.id.toString());
-                  //                           },
-                  //                         ),
-                  //                       );
-                  //                     }).toList()
-                  //                   : movieController.listRamaiReview.sublist(0, 10).map((element) {
-                  //                       return Padding(
-                  //                         padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 24),
-                  //                         child: WidthMovieCard(
-                  //                           element,
-                  //                           onTap: () {
-                  //                             print(element.id.toString());
-                  //                             movieController.fetchDetailMovie(element.id.toString());
-                  //                           },
-                  //                         ),
-                  //                       );
-                  //                     }).toList(),
-                  //             ),
-                  //           ),
-                  //         ],
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
-                  // SizedBox(
-                  //   height: 24,
-                  // ),
+                  Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: 6.w, bottom: 0.h),
+                          child: Text('Upcoming',
+                              style: whiteTextFont.copyWith(fontSize: Get.height * 0.024, fontWeight: FontWeight.w500)),
+                        ),
+                        Obx(() {
+                          return !movieController.isLoading.value
+                              ? Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(18),
+                                  ),
+                                  height: 26.h,
+                                  child: ListView.builder(
+                                      itemCount: movieController.listUpcoming.length,
+                                      scrollDirection: Axis.horizontal,
+                                      itemBuilder: (ctx, index) {
+                                        return Container(
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(18),
+                                          ),
+                                          width: 50.w,
+                                          margin: EdgeInsets.only(
+                                              left: (index == 0) ? 24 : 0,
+                                              right: (index == movieController.listUpcoming.length - 1) ? 20 : 16),
+                                          child: MediumPosterCard(
+                                            movie: movieController.listUpcoming[index],
+                                            onTap: () {
+                                              movieController.fetchDetailMovie(
+                                                  movieController.listUpcoming[index].id.toString(),
+                                                  isNowPlaying: true);
+                                              print(movieController.listUpcoming[index].id);
+                                            },
+                                          ),
+                                        );
+                                      }),
+                                )
+                              : Shimmer.fromColors(
+                                  child: CarouselSlider.builder(
+                                    itemCount: 3,
+                                    itemBuilder: (context, index, relIdx) {
+                                      return Container(
+                                        margin: EdgeInsets.only(
+                                            left: (index == 0) ? 0 : 0, right: (index == 3 - 1) ? 20 : 16),
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                              color: Colors.white10,
+                                              borderRadius: BorderRadius.all(Radius.circular(8))),
+                                          height: Get.height * 0.3,
+                                          width: Get.width * 0.2,
+                                        ),
+                                      );
+                                    },
+                                    options: CarouselOptions(
+                                      height: Get.height * 0.4,
+                                      enableInfiniteScroll: false,
+                                      viewportFraction: 0.60,
+                                      disableCenter: true,
+                                      enlargeCenterPage: true,
+                                      initialPage: 0,
+                                      onPageChanged: (index, reason) {},
+                                      scrollDirection: Axis.horizontal,
+                                    ),
+                                  ),
+                                  baseColor: Colors.white60,
+                                  highlightColor: Colors.grey[300]!,
+                                );
+                        })
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ],
--- lib/app/modules/movie/controllers/movie_controller.dart
+++ lib/app/modules/movie/controllers/movie_controller.dart
@@ -1,20 +1,25 @@
 import 'package:get/get.dart';
+import 'package:youtap_movie/app/data/credits_movie_model.dart';
+import 'package:youtap_movie/app/data/detail_movie_model.dart';
 import 'package:youtap_movie/app/data/movie_model.dart';
+import 'package:youtap_movie/app/modules/movie/controllers/review_controller.dart';
+import 'package:youtap_movie/app/modules/movie/views/detail_movie_view.dart';
 import 'package:youtap_movie/app/utils/network_helper.dart';
 import 'package:youtap_movie/app/utils/url_list_services.dart';
 
 class MovieController extends GetxController {
   var listNowPlaying = <Movie>[].obs;
   var listPopular = <Movie>[].obs;
-  // var searchList = <Movie>[].obs;
-  // var oldSearch = <Movie>[].obs;
-  // var listRamaiReview = <Movie>[].obs;
-  // var detailMovie = DetailMovie().obs;
+  var listUpcoming = <Movie>[].obs;
+  var listCredits = CreditsMovie().obs;
+
+  var detailMovie = DetailMovie().obs;
   var isLoading = false.obs;
   var key = "".obs;
   var isMoreDataAvailable = true.obs;
   // var reviewController = Get.put(ReviewController());
 
+  ReviewController reviewController = Get.put(ReviewController());
   var network = Get.put(NetworkHelper());
 
   @override
@@ -65,26 +70,66 @@ class MovieController extends GetxController {
         });
   }
 
-  // fetchRamaiReview() async {
-  //   isLoading(true);
-  //   List<Movie> result = await MovieServices.getRamaiReview();
-  //   listRamaiReview.assignAll(result);
-  //   isLoading(false);
-  // }
+  fetchUpcoming() async {
+    isLoading(true);
+    return network.get(
+        path: UrlListService.urlUpcomingMovie,
+        queryParameters: {
+          'api_key': '5bc459b70dbb68f19b8106667ecc57c8',
+          'language': 'en-US',
+          'page': '1',
+        },
+        onSuccess: (content) {
+          print('response : $content');
+          List<Movie> result = (content as Iterable).map((e) => Movie.fromJson(e)).toList();
 
-  // fetchDetailMovie(String movieId, {bool isNowPlaying = false}) async {
-  //   DetailMovie result = await MovieServices.getDetailMovie(movieId);
-  //   // print('asdasd' + result.id.toString());
-  //   reviewController.fetchReviewMovie(result.id.toString());
-  //   Get.to(DetailMoviePage(
-  //     isNowPlaying: isNowPlaying,
-  //   ));
-  //   isLoading(true);
+          listUpcoming.assignAll(result.where((element) => element.posterPath != ""));
+          isLoading(false);
+        },
+        onError: (content) {
+          print('error : $content');
+        });
+  }
 
-  //   detailMovie.value = result;
-  //   if (detailMovie.value != null) {}
-  //   isLoading(false);
-  // }
+  fetchDetailMovie(String movieId, {bool isNowPlaying = false}) async {
+    return network.get(
+        path: UrlListService.baseUrl + movieId,
+        isRawResult: true,
+        queryParameters: {'api_key': '5bc459b70dbb68f19b8106667ecc57c8', 'language': 'en-US'},
+        onSuccess: (content) async {
+          print('response : $content');
+
+          await network.get(
+              path: UrlListService.baseUrl + movieId + '/credits',
+              queryParameters: {'api_key': '5bc459b70dbb68f19b8106667ecc57c8', 'language': 'en-US'},
+              isRawResult: true,
+              onSuccess: (credit) {
+                print('credits response : $credit');
+                listCredits.value = CreditsMovie.fromJson(credit);
+                detailMovie.value = DetailMovie.fromJson(content);
+                reviewController.fetchReview(movieId);
+
+                Get.to(DetailMovieView());
+              },
+              onError: (credit) {
+                print('credits error : $credit');
+              });
+        },
+        onError: (content) {
+          print('error : $content');
+        });
+    // DetailMovie result = await MovieServices.getDetailMovie(movieId);
+    // // print('asdasd' + result.id.toString());
+    // reviewController.fetchReviewMovie(result.id.toString());
+    // Get.to(DetailMovieView(
+    //   isNowPlaying: isNowPlaying,
+    // ));
+    // isLoading(true);
+
+    // detailMovie.value = result;
+    // if (detailMovie.value != null) {}
+    // isLoading(false);
+  }
 
   // getSearchMovie(String keywordSearch, int page) async {
   //   try {
